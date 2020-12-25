44000c561053149c4a99b6580183bad2d4ff9f5c
esklyarenkoDS <76124328+esklyarenkoDS@users.noreply.github.com>
2020-12-26 02:41:17 +0300

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
+#!/usr/bin/env bash
+ sudo apt-get -y update
+ sudo apt-get -y install ruby-full ruby-bundler build-essential git
+ 
